2db67bfd0bc9d22dff5f57127bdb11df6fce95c6
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-08-12 20:59:32 +0500

    1.3 task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length == 0) {
+    str = '';
+    return str;
+  } else {
+    let first = str[0].toUpperCase();
+    let second = str.slice(1, str.length);
+    str = first + second;
+    return str;
+  }
 }
