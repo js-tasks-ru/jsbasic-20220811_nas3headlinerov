0536f015367d398d37d9bfb1b633f645f0a8f57e
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-08-18 23:10:12 +0500

    2.1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (!isFinite(salaries[key])) {
+      continue;
+    } else if (typeof(salaries[key]) == 'number') {
+      sum = sum + salaries[key];
+    }      
+  }
+  return sum;
 }
