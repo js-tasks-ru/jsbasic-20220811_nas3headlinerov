acd6568f9044aa473b3af54e57737894fdb56eb9
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-08-28 21:57:58 +0500

    3.3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split('-');
+  for (let i = 0; i < str.length; i++) {      
+    let result = str[i];
+    if (i > 0) {
+      let first = str[i][0].toUpperCase();  
+      let second = str[i].slice(1, str[i].length);
+      result = first + second;
+    }
+    str[i] = result;        
+  }
+  str = str.join('');
+  return str;
 }
