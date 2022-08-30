7c15b87fb5609218f8f8815e6647ba33c1693870
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-08-30 18:27:31 +0500

    3.4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filter = users.filter((elem) => elem.age <= age);
+  let result = [];
+  for (let elem of users) {    
+    result = filter.map(elem => `${elem.name}, ${elem.balance}`)
+    .join('\n');
+  }
+  return result;
 }
