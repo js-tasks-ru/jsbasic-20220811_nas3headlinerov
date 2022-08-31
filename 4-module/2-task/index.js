16732ff66edee9d3459b2762038a27bd7e4996b7
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-08-31 22:37:52 +0500

    4.2 task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.rows;
+  for (let row of rows) {
+    let cells = row.cells;
+    for (let cell of cells) {
+      if (row.rowIndex == cell.cellIndex) {
+        cell.style.backgroundColor = 'red';
+      }
+    }
+  }
 }
