59a4ad71731c3d0414c250001316cf90778d4a46
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-08-31 19:32:42 +0500

    4.1 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let result = friends.map(item => item.firstName + ' ' + item.lastName);
+  
+  document.body.innerHTML = '<ul></ul>';
+  let list = document.querySelector('ul');  
+  for (let name of result) {
+    list.insertAdjacentHTML('beforeEnd', `<li>${name}</li>`);
+  }
+  return list;
 }
