5e51d6d170461af2b2dedd9b8aff0d8ac7ae2028
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-09-06 12:49:41 +0500

    5.3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const inner = document.querySelector('.carousel__inner');
+  let counter = 1;
+
+  function checkCounter() {
+    if (counter == 4) {
+      arrowRight.style.display = 'none';
+    } else if (counter == 1) {
+      arrowLeft.style.display = 'none';
+    } else if (counter > 1 && counter < 4) {
+      arrowRight.style.display = '';
+      arrowLeft.style.display = '';
+    }
+  }
+  checkCounter();
+
+  arrowRight.addEventListener('click', () => { 
+    counter = ++counter;
+    inner.style.transform = `translateX(-${inner.offsetWidth * (counter - 1)}px)`;
+    checkCounter();
+  });
+
+  arrowLeft.addEventListener('click', () => {  
+    counter = --counter;  
+    inner.style.transform = `translateX(-${inner.offsetWidth * (counter - 1)}px)`;
+    checkCounter();
+  });  
 }
