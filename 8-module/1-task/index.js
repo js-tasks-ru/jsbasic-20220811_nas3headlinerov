d7e0cd56389cd6c11218407df11e4d77304fe408
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-09-27 21:01:39 +0500

    8.1 task change4

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -38,12 +38,13 @@ export default class CartIcon {
     window.addEventListener('resize', () => this.updatePosition());
   }
 
-  updatePosition() {
-    let initialTopCoord = this.elem.getBoundingClientRect().top;  
-    let firstElementOffset = document.querySelector('.container').getBoundingClientRect().right + 20;
-    let rightOffsetScreen = document.documentElement.clientWidth - this.elem.offsetWidth - 10;
-    let minOffset = Math.min(firstElementOffset, rightOffsetScreen) + 'px';
-    if (this.elem.offsetWidth && document.documentElement.clientWidth > 767) {      
+  updatePosition() {    
+    if (this.elem.offsetWidth && document.documentElement.clientWidth > 767) {
+      let initialTopCoord = this.elem.getBoundingClientRect().top;  
+      let containerRight = document.querySelector('.container').getBoundingClientRect().right;
+      let firstElementOffset = containerRight + 20;
+      let rightOffsetScreen = document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+      let minOffset = Math.min(firstElementOffset, rightOffsetScreen) + 'px';      
       if (document.documentElement.scrollTop > initialTopCoord) {
         Object.assign(this.elem.style, {
           position: 'fixed',
