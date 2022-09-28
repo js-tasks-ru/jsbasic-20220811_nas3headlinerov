45a0ab8e140488ea106d375cefe392e3107fdfd5
Georgy Shagin <nas3headlinerov@yandex.ru>
2022-09-28 17:38:13 +0500

    8.2 task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -4,6 +4,44 @@ import ProductCard from '../../6-module/2-task/index.js';
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
-    this.filters = {};
+    this.filters = {};    
+    this.render();
+    this.selector();
+    this.renderCard();
+  }
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `); 
+  }
+  selector(selector) {
+    return this.elem.querySelector(`${selector}`);
+  }
+  renderCard() {
+    let inner = this.selector('.products-grid__inner');
+    inner.innerHTML = '';
+    let products = this.products;    
+    for (let product of products) {
+      if (this.filters.noNuts && product.nuts) { /* должны получить для применения фильтра NO */
+        continue;
+      }
+      if (this.filters.vegeterianOnly && !product.vegeterian) {
+        continue;
+      }
+      if (this.filters.maxSpiciness < product.spiciness) {
+        continue;
+      }
+      if (this.filters.category !== product.category && this.filters.category) {
+        continue;
+      }
+      let card = new ProductCard(product);
+      inner.append(card.elem);
+    }
+  }
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.renderCard();
   }
 }
